541531eff0205bc88c092eb8b095afd25fa254aa
apple <apple@macs-MacBook-Pro-3.local>
2018-09-30 18:25:36 +0700

    Rating Component

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -1,33 +1,51 @@
-import React, { Component } from 'react';
 import React, { Component } from "react";
-import { Container, Header, Content, Card, CardItem, Text, Body } from "native-base";
-export default class CardItemBordered extends Component {
+import { View } from 'react-native'
+import { Container, Header, Content, Card, CardItem, Text, Body, Icon, List, ListItem } from "native-base";
+
+
+const CONTOH_DATA = ['satu', 'dua', 'tiga']
+
+
+export default class ItemCardRating extends Component {
+  static defaultProps = {
+    title : 'Jalan-jalan Bandung',
+    rating: 4.3,
+    items : ['Gedung Sate', 'Gasibu', 'UpNormal'],
+    onClickCopy: () => alert('clicked')
+  }
+
   render() {
     return (
-      <Container>
-        <Header />
-        <Content padder>
-          <Card>
-            <CardItem header bordered>
-              <Text>NativeBase</Text>
+        <View style={{margin:10}}>
+          <Card >
+            <CardItem header>
+              <Text style={{flex:1}}>{this.props.title}</Text>
+              <View style={{flexDirection:'row', alignSelf:'flex-end'}}>
+                <Icon type="MaterialIcons" name="star" />
+                <Text style={{textAlignVertical:'center'}}>{this.props.rating}</Text>
+              </View>
             </CardItem>
             <CardItem bordered>
               <Body>
-                <Text>
-                  NativeBase is a free and open source framework that enable
-                  developers to build
-                  high-quality mobile apps using React Native iOS and Android
-                  apps
-                  with a fusion of ES6.
-                </Text>
-              </Body>
+                <List 
+                  style={{ width:'100%'}}
+                  dataArray={this.props.items}
+                  renderRow={(item)=> 
+                    <ListItem >
+                      <Icon type="MaterialIcons" name="keyboard-arrow-right"/>
+                      <Text>{item}</Text>
+                    </ListItem>
+                  }
+                >
+                </List>
+                </Body>
             </CardItem>
-            <CardItem footer bordered>
-              <Text>GeekyAnts</Text>
+            <CardItem footer bordered style={{justifyContent:'center'}} button onPress={this.props.onClickCopy}>
+              <Text>Copy</Text>
             </CardItem>
           </Card>
-        </Content>
-      </Container>
+          </View>
+
     );
   }
 }
